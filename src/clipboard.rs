cdd8d9965c2a641572ff0711f9d54cb38f38cd38
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-09-18 22:28:02 +0330

    minor: cleanup useless docs

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,19 +1,19 @@
 use std::sync::{Arc, Mutex};
 #[cfg(target_os = "linux")]
-use std::thread; // Import Arc and Mutex
+use std::thread;
 
 #[cfg(target_os = "linux")]
 use arboard::SetExtLinux;
 use arboard::{Clipboard, Error};
 
 pub struct EditorClipboard {
-    clipboard: Arc<Mutex<Clipboard>>, // Wrap Clipboard in Arc<Mutex>
+    clipboard: Arc<Mutex<Clipboard>>,
 }
 
 impl EditorClipboard {
     pub fn new() -> Result<EditorClipboard, Error> {
         Clipboard::new().map(|c| EditorClipboard {
-            clipboard: Arc::new(Mutex::new(c)), // Initialize with Arc<Mutex>
+            clipboard: Arc::new(Mutex::new(c)),
         })
     }
 
@@ -23,22 +23,22 @@ impl EditorClipboard {
 
     #[cfg(not(target_os = "linux"))]
     pub fn set_contents(&mut self, content: String) -> Result<(), Error> {
-        let mut clipboard = self.clipboard.lock().unwrap(); // Lock the clipboard
+        let mut clipboard = self.clipboard.lock().unwrap();
         clipboard.set_text(content)
     }
 
     #[cfg(target_os = "linux")]
     pub fn set_contents(&mut self, content: String) -> Result<(), Error> {
-        let clipboard = Arc::clone(&self.clipboard); // Clone the Arc
+        let clipboard = Arc::clone(&self.clipboard);
         thread::spawn(move || {
-            let mut clipboard = clipboard.lock().unwrap(); // Lock the clipboard inside the thread
+            let mut clipboard = clipboard.lock().unwrap();
             clipboard.set().wait().text(content).unwrap();
         });
         Ok(())
     }
 
     pub fn get_content(&mut self) -> Result<String, Error> {
-        let mut clipboard = self.clipboard.lock().unwrap(); // Lock the clipboard
+        let mut clipboard = self.clipboard.lock().unwrap();
         clipboard.get_text()
     }
 }
